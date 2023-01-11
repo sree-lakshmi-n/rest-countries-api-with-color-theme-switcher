762fdecea649c428865049f7ddf267f6a0f09c87
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-11 14:44:14 +0530

    Header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,19 @@
 import React from "react";
+import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
+import ThemeBtn from "../ThemeBtn/ThemeBtn";
+import "./Header.css";
 
-export default function Header() {
-  return <div>Header</div>;
+export default function Header(props) {
+  return (
+    <div
+      className={`header ${
+        props.isDarkMode ? "header__dark" : "header__light"
+      }`}
+    >
+      <FlexWrapper className="header-content">
+        <h1 className="header__title">Where in the world?</h1>
+        <ThemeBtn />
+      </FlexWrapper>
+    </div>
+  );
 }
