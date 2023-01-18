0036420fcebba270aca7e6932146104a193b3176
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-18 10:04:02 +0530

    CountryCardWrapper component added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import CountryCard from "../../Components/CountryCard/CountryCard";
+import CountryCardWrapper from "../../Components/CountryCardWrapper/CountryCardWrapper";
 import Header from "../../Components/Header/Header";
 import "./Home.css";
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+
   const themeChangeHandler = (theme) => {
     setIsDarkMode(theme);
   };
   return (
     <div className={`container ${isDarkMode ? "dark-theme" : "light-theme"}`}>
       <Header onThemeChange={themeChangeHandler} theme={isDarkMode} />
-      <CountryCard />
+      <CountryCardWrapper theme={isDarkMode} />
     </div>
   );
 }
