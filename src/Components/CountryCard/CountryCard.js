2b544691544c14a7fc05eade35bae01be4fa767d
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-18 10:01:47 +0530

    displaying data using props

--- src/Components/CountryCard/CountryCard.js
+++ src/Components/CountryCard/CountryCard.js
@@ -2,30 +2,25 @@ import React from "react";
 import FlexWrapper from "../../UI/FlexWrapper/FlexWrapper";
 import "./CountryCard.css";
 
-export default function CountryCard() {
+export default function CountryCard(props) {
   return (
     <FlexWrapper className="country-card">
       <div className="country-flag-wrapper">
-        <img
-          src={
-            "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png"
-          }
-          className="country-flag"
-          alt="country's flag "
-        />
+        <img src={props.flag} className="country-flag" alt="country's flag " />
       </div>
       <div className="country-details-wrapper">
-        <p className="country-name">India</p>
+        <p className="country-name">{props.name}</p>
         <FlexWrapper className="country-details">
           <p className="country-population">
             Population:{" "}
-            <span className="country-population-value">81,770,900</span>
+            <span className="country-population-value">{props.population}</span>
           </p>
           <p className="country-region">
-            Region: <span className="country-region-value">Asia</span>
+            Region: <span className="country-region-value">{props.region}</span>
           </p>
           <p className="country-capital">
-            Capital: <span className="country-capital-value">New Delhi</span>
+            Capital:{" "}
+            <span className="country-capital-value">{props.capital}</span>
           </p>
         </FlexWrapper>
       </div>
