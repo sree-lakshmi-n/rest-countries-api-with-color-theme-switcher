df26a2854ff5bf0668c8a2a935a64731037e786e
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-18 11:22:48 +0530

    FilterWrapper component added with url change functionality

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 import CountryCardWrapper from "../../Components/CountryCardWrapper/CountryCardWrapper";
+import FilterWrapper from "../../Components/FilterWrapper/FilterWrapper";
 import Header from "../../Components/Header/Header";
-import SearchBar from "../../Components/SearchBar/SearchBar";
 import "./Home.css";
 
 export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [url, setUrl] = useState("https://restcountries.com/v2/all");
 
   const themeChangeHandler = (theme) => {
     setIsDarkMode(theme);
   };
+  const urlChangeHandler = (changedUrl) => {
+    setUrl(changedUrl);
+  };
+
   return (
     <div className={`container ${isDarkMode ? "dark-theme" : "light-theme"}`}>
       <Header onThemeChange={themeChangeHandler} theme={isDarkMode} />
-      <SearchBar />
-      <CountryCardWrapper theme={isDarkMode} />
+      <FilterWrapper onInputChange={urlChangeHandler} />
+      <CountryCardWrapper theme={isDarkMode} url={url} />
     </div>
   );
 }
