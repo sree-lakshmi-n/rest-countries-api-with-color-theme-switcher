997611c9459d632447e568bd3a9b10ffdc896244
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-18 11:22:20 +0530

    SearchBar component added. Country list to change on input.

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,6 +1,16 @@
 import "./SearchBar.css";
 import React from "react";
 
-export default function SearchBar() {
-  return <div>SearchBar</div>;
+export default function SearchBar(props) {
+  const inputChangeHandler = (event) => {
+    const countryName = event.target.value;
+    console.log(countryName);
+    props.onInputChange(`https://restcountries.com/v2/name/${countryName}`);
+  };
+  return (
+    <div>
+      <ion-icon name="search-outline"></ion-icon>
+      <input type="text" onInput={inputChangeHandler}></input>
+    </div>
+  );
 }
