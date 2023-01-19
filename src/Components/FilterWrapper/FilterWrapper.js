6a24b8d9f53308a465d2ecb7403f055ddadd0833
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-19 10:46:56 +0530

    checkHandler functionality. RegionFilter and Checkbox components added

--- src/Components/FilterWrapper/FilterWrapper.js
+++ src/Components/FilterWrapper/FilterWrapper.js
@@ -1,11 +1,23 @@
 import "./FilterWrapper.css";
-import React from "react";
+import React, { useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
+import RegionFilter from "../RegionFilter/RegionFilter";
+import Checkbox from "../Checkbox/Checkbox";
 
 export default function FilterWrapper(props) {
+  const checkHandler = (event) => {
+    props.checkList.map((ele) => {
+      if (ele.param == event.target.value) {
+        ele.status = !ele.status;
+        props.onCheckboxInput(props.checkList);
+      }
+    });
+  };
   return (
     <div>
       <SearchBar onInputChange={props.onInputChange} />
+      <RegionFilter onInputChange={props.onInputChange} />
+      <Checkbox onChecked={checkHandler} params={props.checkList} />
     </div>
   );
 }
