427ce4a1032a5c256bd66e8c3f11e6aea81cfb01
sree-lakshmi-n <sreelakshmin153@gmai.com>
2023-01-19 11:42:37 +0530

    added state variables, fetch url using useEffect, and props to CountryCardWrapper and FilterWrapper components

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CountryCardWrapper from "../../Components/CountryCardWrapper/CountryCardWrapper";
 import FilterWrapper from "../../Components/FilterWrapper/FilterWrapper";
 import Header from "../../Components/Header/Header";
@@ -8,18 +8,45 @@ export default function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
   const [url, setUrl] = useState("https://restcountries.com/v2/all");
 
+  const [countriesList, setCountriesList] = useState("");
+
+  const [checkList, setCheckList] = useState([
+    { param: "currencies", status: false },
+    { param: "languages", status: false },
+    { param: "borders", status: false },
+  ]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setCountriesList(data);
+      });
+  }, [url, checkList]);
+
   const themeChangeHandler = (theme) => {
     setIsDarkMode(theme);
   };
   const urlChangeHandler = (changedUrl) => {
     setUrl(changedUrl);
   };
-
+  const checkboxInputHandler = (checks) => {
+    setCheckList([...checks]);
+  };
   return (
     <div className={`container ${isDarkMode ? "dark-theme" : "light-theme"}`}>
       <Header onThemeChange={themeChangeHandler} theme={isDarkMode} />
-      <FilterWrapper onInputChange={urlChangeHandler} />
-      <CountryCardWrapper theme={isDarkMode} url={url} />
+      <FilterWrapper
+        onInputChange={urlChangeHandler}
+        checkList={checkList}
+        onCheckboxInput={checkboxInputHandler}
+      />
+      <CountryCardWrapper
+        theme={isDarkMode}
+        url={url}
+        countriesList={countriesList}
+        checkList={checkList}
+      />
     </div>
   );
 }
